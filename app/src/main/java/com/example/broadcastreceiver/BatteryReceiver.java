5682bd5ff8b3f117189317086928618d8b8f30e5
eazurin <eazurin@unsa.edu.pe>
2024-10-01 17:30:58 -0500

    Interfaz grafica y clase BatteryReceiver

---
+++ app/src/main/java/com/example/broadcastreceiver/BatteryReceiver.java
@@ -0,0 +1,33 @@
+package com.example.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.TextView;
+
+public class BatteryReceiver extends BroadcastReceiver {
+
+    private TextView batteryStatusTextView;
+    private static final String TAG = "BatteryReceiver";
+
+    public BatteryReceiver(TextView batteryStatusTextView) {
+        this.batteryStatusTextView = batteryStatusTextView;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        int level = intent.getIntExtra("level", -1);
+        int scale = intent.getIntExtra("scale", -1);
+
+
+        int batteryPct = (int) ((level / (float) scale) * 100);
+
+
+        batteryStatusTextView.setText("Estado de la batería: " + batteryPct + "%");
+
+       
+        Log.d(TAG, "Nivel de batería recibido: " + batteryPct + "%");
+    }
+}
